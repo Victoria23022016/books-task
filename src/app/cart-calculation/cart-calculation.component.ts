054565ce4d9f3d84413a972f14dbb795c147f026
Vika <svi.victoria@gmail.com>
2023-10-19 20:59:42 +0700

    Добавила компонент расчета итоговой стоимости

--- src/app/cart-calculation/cart-calculation.component.ts
+++ src/app/cart-calculation/cart-calculation.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../book.service';
 
 @Component({
   selector: 'app-cart-calculation',
   templateUrl: './cart-calculation.component.html',
-  styleUrls: ['./cart-calculation.component.css']
+  styleUrls: ['./cart-calculation.component.css'],
 })
-export class CartCalculationComponent {
+export class CartCalculationComponent implements OnInit {
+  @Input() books: Book[];
 
+  totalCost: number;
+
+  ngOnInit(): void {
+    this.totalCost = this.calcTotalCost();
+  }
+
+  calcTotalCost(): number {
+    //подумать, может вынести в сервис этот метод?
+    return this.books
+      .map((el) => el.price)
+      .map((el) => +el.slice(1, el.length))
+      .reduce((sum, number) => {
+        return sum + number;
+      }, 0);
+  }
 }
